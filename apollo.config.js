63f668fb01939cde492c4be9303664e09427bf66
Jimmy Sergio <jimmy1sergio2@gmail.com>
2022-09-26 13:59:31 +0200

    changed from http to https

--- apollo.config.js
+++ apollo.config.js
@@ -3,7 +3,7 @@ module.exports = {
     service: {
       name: "my-app",
       // URL to the GraphQL API
-      url: "http://dolphin-app-ewqob.ondigitalocean.app/graphql",
+      url: "https://dolphin-app-ewqob.ondigitalocean.app/graphql",
     },
     // Files processed by the extension
     includes: ["src/**/*.vue", "src/**/*.js"],
