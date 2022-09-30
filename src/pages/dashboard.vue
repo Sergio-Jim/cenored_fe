56a0b514354f18ac6832f0803b017b2c65bf567b
Jimmy Sergio <jimmy1sergio2@gmail.com>
2022-09-30 02:54:06 +0200

    working on series update

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -53,6 +53,7 @@
               height="350"
               :options="chartOptions"
               :series="series"
+              ref="statistics"
             ></apexchart>
           </div>
           <div>
@@ -100,28 +101,29 @@ export default {
   name: "DashboardLanding",
   data() {
     return {
-      series: [
-        {
-          name: "Disappointed",
-          data: [44, 55, 41, 37, 22, 43, 21, 40, 51],
-        },
-        {
-          name: "Slightly disappointed",
-          data: [53, 32, 33, 52, 13, 43, 32, 49, 28],
-        },
-        {
-          name: "Okay",
-          data: [12, 17, 11, 9, 15, 11, 20, 8, 13],
-        },
-        {
-          name: "Good",
-          data: [9, 7, 5, 8, 6, 9, 4, 5, 3],
-        },
-        {
-          name: "Exceptional",
-          data: [25, 12, 19, 32, 25, 24, 10, 21, 8],
-        },
-      ],
+      allStats: {},
+      // series: [
+      //   {
+      //     name: "Disappointed",
+      //     data: [44, 55, 41, 37, 22, 43, 21, 40, 51],
+      //   },
+      //   {
+      //     name: "Slightly disappointed",
+      //     data: [53, 32, 33, 52, 13, 43, 32, 49, 28],
+      //   },
+      //   {
+      //     name: "Okay",
+      //     data: [12, 17, 11, 9, 15, 11, 20, 8, 13],
+      //   },
+      //   {
+      //     name: "Good",
+      //     data: [9, 7, 5, 8, 6, 9, 4, 5, 3],
+      //   },
+      //   {
+      //     name: "Exceptional",
+      //     data: [25, 12, 19, 32, 25, 24, 10, 21, 8],
+      //   },
+      // ],
       chartOptions: {
         chart: {
           type: "bar",
@@ -185,6 +187,198 @@ export default {
   components: {
     apexchart: VueApexCharts,
   },
+  created() {
+    this.$apollo
+      .query({
+        query: gql`
+          query getStatistics {
+            getStatistics {
+              question1_Exceptional
+              question1_Good
+              question1_Okay
+              question1_SlightlyDisappointed
+              question1_Disappointed
+              question2_Exceptional
+              question2_Good
+              question2_Okay
+              question2_SlightlyDisappointed
+              question2_Disappointed
+              question3_Exceptional
+              question3_Good
+              question3_Okay
+              question3_SlightlyDisappointed
+              question3_Disappointed
+              question4_Exceptional
+              question4_Good
+              question4_Okay
+              question4_SlightlyDisappointed
+              question4_Disappointed
+              question5_Exceptional
+              question5_Good
+              question5_Okay
+              question5_SlightlyDisappointed
+              question5_Disappointed
+              question6_Exceptional
+              question6_Good
+              question6_Okay
+              question6_SlightlyDisappointed
+              question6_Disappointed
+              question7_Exceptional
+              question7_Good
+              question7_Okay
+              question7_SlightlyDisappointed
+              question7_Disappointed
+              question8_Exceptional
+              question8_Good
+              question8_Okay
+              question8_SlightlyDisappointed
+              question8_Disappointed
+              question9_Exceptional
+              question9_Good
+              question9_Okay
+              question9_SlightlyDisappointed
+              question9_Disappointed
+            }
+          }
+        `,
+      })
+      .then(({ data }) => {
+        var stats = data.getStatistics;
+        this.allStats = data.getStatistics;
+
+        this.$refs.statistics.updateSeries([
+          {
+            name: "question 1",
+            type: "column",
+            data: stats.map((stat) => {
+              console.log(
+                stat.question1_Exceptional,
+                stat.question1_Good,
+                stat.question1_Okay,
+                stat.question1_SlightlyDisappointed,
+                stat.question1_Disappointed
+              );
+              return (
+                stat.question1_Exceptional,
+                stat.question1_Good,
+                stat.question1_Okay,
+                stat.question1_SlightlyDisappointed,
+                stat.question1_Disappointed
+              );
+            }),
+          },
+          // {
+          //   name: "question 2",
+          //   type: "column",
+          //   data: stats.map((stat) => {
+          //     return [
+          //       stat.question2_Exceptional,
+          //       stat.question2_Good,
+          //       stat.question2_Okay,
+          //       stat.question2_SlightlyDisappointed,
+          //       stat.question2_Disappointed,
+          //     ];
+          //   }),
+          // },
+          // {
+          //   name: "question 3",
+          //   type: "column",
+          //   data: stats.map((stat) => {
+          //     return [
+          //       stat.question3_Exceptional,
+          //       stat.question3_Good,
+          //       stat.question3_Okay,
+          //       stat.question3_SlightlyDisappointed,
+          //       stat.question3_Disappointed,
+          //     ];
+          //   }),
+          // },
+          // {
+          //   name: "question 4",
+          //   type: "column",
+          //   data: stats.map((stat) => {
+          //     return [
+          //       stat.question4_Exceptional,
+          //       stat.question4_Good,
+          //       stat.question4_Okay,
+          //       stat.question4_SlightlyDisappointed,
+          //       stat.question4_Disappointed,
+          //     ];
+          //   }),
+          // },
+          // {
+          //   name: "question 5",
+          //   type: "column",
+          //   data: stats.map((stat) => {
+          //     return [
+          //       stat.question5_Exceptional,
+          //       stat.question5_Good,
+          //       stat.question5_Okay,
+          //       stat.question5_SlightlyDisappointed,
+          //       stat.question5_Disappointed,
+          //     ];
+          //   }),
+          // },
+          // {
+          //   name: "question 6",
+          //   type: "column",
+          //   data: stats.map((stat) => {
+          //     return [
+          //       stat.question6_Exceptional,
+          //       stat.question6_Good,
+          //       stat.question6_Okay,
+          //       stat.question6_SlightlyDisappointed,
+          //       stat.question6_Disappointed,
+          //     ];
+          //   }),
+          // },
+          // {
+          //   name: "question 7",
+          //   type: "column",
+          //   data: stats.map((stat) => {
+          //     return [
+          //       stat.question7_Exceptional,
+          //       stat.question7_Good,
+          //       stat.question7_Okay,
+          //       stat.question7_SlightlyDisappointed,
+          //       stat.question7_Disappointed,
+          //     ];
+          //   }),
+          // },
+          // {
+          //   name: "question 8",
+          //   type: "column",
+          //   data: stats.map((stat) => {
+          //     return [
+          //       stat.question8_Exceptional,
+          //       stat.question8_Good,
+          //       stat.question8_Okay,
+          //       stat.question8_SlightlyDisappointed,
+          //       stat.question8_Disappointed,
+          //     ];
+          //   }),
+          // },
+          // {
+          //   name: "question 9",
+          //   type: "column",
+          //   data: stats.map((stat) => {
+          //     return [
+          //       stat.question9_Exceptional,
+          //       stat.question9_Good,
+          //       stat.question9_Okay,
+          //       stat.question9_SlightlyDisappointed,
+          //       stat.question9_Disappointed,
+          //     ];
+          //   }),
+          // },
+        ]);
+      })
+      .catch((err) => {
+        this.toast.error(err.message || "Something went wrong", {
+          timeout: 2000,
+        });
+      });
+  },
 };
 </script>
 
@@ -201,3 +395,55 @@ export default {
   height: 100%;
 }
 </style>
+
+<!-- gql`
+          query getStatistics {
+            getStatistics {
+              question1_Exceptional
+              question1_Good
+              question1_Okay
+              question1_SlightlyDisappointed
+              question1_Disappointed
+              question2_Exceptional
+              question2_Good
+              question2_Okay
+              question2_SlightlyDisappointed
+              question2_Disappointed
+              question3_Exceptional
+              question3_Good
+              question3_Okay
+              question3_SlightlyDisappointed
+              question3_Disappointed
+              question4_Exceptional
+              question4_Good
+              question4_Okay
+              question4_SlightlyDisappointed
+              question4_Disappointed
+              question5_Exceptional
+              question5_Good
+              question5_Okay
+              question5_SlightlyDisappointed
+              question5_Disappointed
+              question6_Exceptional
+              question6_Good
+              question6_Okay
+              question6_SlightlyDisappointed
+              question6_Disappointed
+              question7_Exceptional
+              question7_Good
+              question7_Okay
+              question7_SlightlyDisappointed
+              question7_Disappointed
+              question8_Exceptional
+              question8_Good
+              question8_Okay
+              question8_SlightlyDisappointed
+              question8_Disappointed
+              question9_Exceptional
+              question9_Good
+              question9_Okay
+              question9_SlightlyDisappointed
+              question9_Disappointed
+            }
+          }
+        ` -->
